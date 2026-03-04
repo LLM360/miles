3a3babe18095f9bd87eb79ecaf074773877bb79b
fzyzcjy <ch271828n@outlook.com>
2026-03-04 18:41:07 +0800

    fix: remove eager imports of K8sNodeManager/RayTrainingJob from __init__
    These have heavy dependencies (kubernetes_asyncio, ray, structlog) that
    should not be required at import time. Users import them directly from
    their modules when needed.

--- miles/utils/ft/platform/__init__.py
+++ miles/utils/ft/platform/__init__.py
@@ -1,19 +1,15 @@
-from miles.utils.ft.platform.k8s_node_manager import K8sNodeManager
 from miles.utils.ft.platform.protocols import (
     JobStatus,
     NodeManagerProtocol,
     NotificationProtocol,
     TrainingJobProtocol,
 )
-from miles.utils.ft.platform.ray_training_job import RayTrainingJob
 from miles.utils.ft.platform.stubs import StubNodeManager, StubTrainingJob
 
 __all__ = [
     "JobStatus",
-    "K8sNodeManager",
     "NodeManagerProtocol",
     "NotificationProtocol",
-    "RayTrainingJob",
     "StubNodeManager",
     "StubTrainingJob",
     "TrainingJobProtocol",
